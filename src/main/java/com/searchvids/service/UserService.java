111daf86e010492d8075863c8f3c3943f3de536d
bwc0 <bryantcason32@gmail.com>
2019-06-18 15:33:50 -0400

    added remove video from favorites end point and test

--- src/main/java/com/searchvids/service/UserService.java
+++ src/main/java/com/searchvids/service/UserService.java
@@ -8,5 +8,6 @@ public interface UserService {
     ResponseMessage findUserById(Long id);
     ResponseMessage updateUser(Long id, User user);
     void addVideoToUserVideoList(Long id, Video video);
+    void removeVideoFromUserVideoList(Long id, String videoId);
     void deleteUserById(Long id);
 }
